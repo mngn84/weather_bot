a0e9ddf0c6a5ae892eb1fcd9d0dacc05ed7c96e6
mngn84 <mngn84@gmail.com>
2024-10-20 17:38:30 +0400

    logs

--- bot.js
+++ bot.js
@@ -1,17 +1,37 @@
 import TelegramBot from "node-telegram-bot-api";
 import axios from "axios";
 import dotenv from "dotenv";
+import express from 'express';
 
 dotenv.config();
 
-export const bot = new TelegramBot(process.env.BOT_TOKEN, { webHook: true });
+const app = express();
+const port = process.env.PORT;
+console.log('app created');
+
+app.use(express.json());
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { webHook: true });
 bot.setWebHook(`${process.env.APP_URL}/bot${process.env.BOT_TOKEN}`);
 
 console.log('Bot is running...');
+app.get('/', (_, res) => {
+    console.log('get /: ');
+        // bot.sendMessage(5671085312, 'Server is running');
+        res.send('Server is running');
+    // res.end('Server is running');
+});
+
+app.post(`/bot${process.env.BOT_TOKEN}`, (req, res) => {
+    console.log('post /bot: ');
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
 
 const cachedCoords = new Map();
 
 bot.on('message', (msg) => {
+    console.log('bot.on:', msg.text);
     if (msg.text === '/start') {
         bot.sendMessage(msg.chat.id, "Привет! Я бот погоды. Введите название города, чтобы узнать погоду в нём.");
     } else if (/^[A-Za-zА-Яа-я](?:[\s-][A-Za-zА-Яа-я0-9]+)*/.test(msg.text)) {
@@ -61,7 +81,7 @@ const getCoords = async (msg) => {
                 appid: process.env.API_KEY,
             },
         });
-        
+
         const { lat, lon } = geoResponse.data[0];
         return { lat, lon };
     } catch (error) {
@@ -69,3 +89,6 @@ const getCoords = async (msg) => {
     }
 }
 
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-import express from 'express';
+/* import express from 'express';
 import dotenv from 'dotenv';
-import { bot } from './bot.js';
+import { bot, handleMessage } from './bot.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+console.log('app created');
 
 app.use(express.json());
 
 app.get('/', (_, res) => {
+    bot.sendMessage(5671085312, 'Server is running');
+    console.log('get /: ');
+    res.send('Hello World!');
     res.end();
 });
 
 app.post(`/bot${process.env.BOT_TOKEN}`, (req, res) => {
+    console.log('post /bot: ', req.body);
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
 
+handleMessage(bot);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+ */
